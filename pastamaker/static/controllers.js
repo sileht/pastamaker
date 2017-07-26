17e4a1d809d0667cab0839dd67b5f250cb225786
Mehdi Abaakouk <sileht@sileht.net>
2017-07-26 15:46:03 +0200

    Add travis status in pastamaker UI

--- pastamaker/static/controllers.js
+++ pastamaker/static/controllers.js
@@ -16,6 +16,7 @@ app.classy.controller({
         this.$scope.counter = 0;
         this.$scope.autorefresh = false;
         this.$scope.event = false;
+        this.$scope.current_travis_raw_open = null;
 
         if(typeof(EventSource) !== "undefined") {
             console.log("event enabled");
@@ -56,11 +57,59 @@ app.classy.controller({
         this.$scope.last_update = new Date();
         this.$scope.refreshing = false;
         this.$scope.counter = this.refresh_interval;
+        if (this.$scope.current_travis_raw_open) {
+            this.toggle_travis_info(this.$scope.current_travis_raw_open);
+        }
     },
     on_error: function(data, status) {
         console.warn(data, status);
         this.$scope.refreshing = false;
         this.$scope.counter = this.refresh_interval;
+    },
+    hide_all_travis_info: function() {
+        this.$scope.groups.forEach(function(group) {
+            group.pulls.forEach(function(pull) {
+                pull.open_travis_row = false;
+            });
+        });
+    },
+    toggle_travis_info: function(pull) {
+        var opened = pull.open_travis_row;
+        this.hide_all_travis_info()
+        if (!opened) {
+            pull.open_travis_row = true;
+            this.$scope.current_travis_raw_open = pull;
+            this.refresh_travis(pull);
+        } else {
+            this.$scope.current_travis_raw_open = null;
+        }
+    },
+    refresh_travis: function(pull) {
+        pull.travis_build = undefined;
+        pull.travis_jobs = [];
+
+        var v2_headers = { "Accept": "application/vnd.travis-ci.2+json" };
+        var travis_base_url = 'https://api.travis-ci.org'
+        this.$http.get(travis_base_url + "/builds?event_type=pull_request&slug=" + pull.base.repo.full_name,
+                       {headers: v2_headers}
+        ).success(function(data, status, headers) {
+            data.builds.forEach(function(build) {
+                // Only keep last build
+                if (pull.travis_build === undefined && build.pull_request_number == pull.number) {
+                    pull.travis_build = build;
+                    pull.travis_jobs = [];
+                    build.job_ids.forEach(function(job_id) {
+                        this.$http.get(travis_base_url + "/jobs/" + job_id,
+                                       {headers: v2_headers}
+                        ).success(function(data, status, headers) {
+                            // console.log(data.job);
+                            pull.travis_jobs.push(data.job);
+                        }.bind(this));
+
+                    }.bind(this))
+                }
+            }.bind(this));
+        }.bind(this));
     },
 })
 ;
