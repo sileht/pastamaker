a0d7be3f7fda9444356aa9419a054e86ab762ae8
Mehdi Abaakouk <sileht@sileht.net>
2017-12-11 17:14:34 +0100

    Fix comments window refresh

--- pastamaker/static/controllers.js
+++ pastamaker/static/controllers.js
@@ -64,10 +64,12 @@ app.classy.controller({
             this.$scope.groups = []
             data.forEach(function(group) {
 
-                var comment_read_to_keep = [];
-                var repo;
+//                var comment_read_to_keep = [];
+//                var repo;
+
                 // reopen tabs
                 group.pulls.forEach(function(pull) {
+                    var repo = pull.base.repo.full_name;
                     if (old_travis_tabs.hasOwnProperty(repo)){
                         if (old_travis_tabs[repo].indexOf(pull.number) !== -1) {
                             this.toggle_travis_info(pull);
@@ -83,18 +85,18 @@ app.classy.controller({
                             this.toggle_comments_info(pull);
                         }
                     }
-                    var cache_key = "comment~" + repo + "~" + pull.number;
-                    pull.comment_read = window.localStorage.getItem(cache_key);
-                    comment_read_to_keep.push(cache_key);
+//                    var cache_key = "comment~" + repo + "~" + pull.number;
+//                    pull.comment_read = window.localStorage.getItem(cache_key);
+//                    comment_read_to_keep.push(cache_key);
                 }.bind(this));
 
-                if (group.pulls.length > 0){
-                    for (var k in window.localStorage) {
-                        if (k.startsWith("comment~" + repo + "~") && ! (k in comment_read_to_keep)) {
-                            window.localStorage.removeImte(k);
-                        }
-                    }
-                }
+//                if (group.pulls.length > 0){
+//                    for (var k in window.localStorage) {
+//                        if (k.startsWith("comment~" + repo + "~") && ! (k in comment_read_to_keep)) {
+//                            window.localStorage.removeImte(k);
+//                        }
+//                    }
+//                }
 
                 this.$scope.groups.push(group)
             }.bind(this));
@@ -137,7 +139,7 @@ app.classy.controller({
                 pull.open_comments_row = true;
             } else {
                 pull.open_comments_row = false;
-                window.localStorage.setItem("comment~" + repo + "~" + pull.number, pull.comments);
+                // window.localStorage.setItem("comment~" + repo + "~" + pull.number, pull.comments);
                 this.opened_comments_tabs[repo] = this.opened_comments_tabs[repo].filter(e => e !== pull.number)
             }
         },
