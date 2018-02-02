1abac36d0cad991d76c5ea31c71c1f73b0220cef
Mehdi Abaakouk <sileht@sileht.net>
2018-02-02 16:37:18 +0100

    Retry on error

--- pastamaker/worker.py
+++ pastamaker/worker.py
@@ -28,6 +28,9 @@ from pastamaker import utils
 
 LOG = logging.getLogger(__name__)
 
+QUEUES = None
+MAX_FAILURES = 3
+
 
 def event_handler(event_type, data):
     """Everything start here"""
@@ -44,10 +47,28 @@ def event_handler(event_type, data):
     except github.RateLimitExceededException:
         LOG.error("rate limit reached")
 
+def retry_handler(job, *exc_info):
+    job.meta.setdefault('failures', 0)
+    job.meta['failures'] += 1
 
-def error_handler(job, *exc_info):
-    pass
-    # LOG.error("event handler failure", exc_info=exc_info)
+    # Too many failures
+    if job.meta['failures'] >= MAX_FAILURES:
+        LOG.warn('job %s: failed too many times times - moving to failed queue' % job.id)
+        job.save()
+        return True
+
+    # Requeue job and stop it from being moved into the failed queue
+    LOG.warn('job %s: failed %d times - retrying' % (job.id, job.meta['failures']))
+
+    for queue in QUEUES:
+        if queue.name == job.origin:
+            queue.enqueue_job(job, timeout=job.timeout)
+            return False
+
+    # Can't find queue, which should basically never happen as we only work jobs that match the given queue names and
+    # queues are transient in rq.
+    LOG.warn('job %s: cannot find queue %s - moving to failed queue' % (job.id, job.origin))
+    return True
 
 
 def main():
@@ -57,8 +78,9 @@ def main():
     if config.FLUSH_REDIS_ON_STARTUP:
         utils.get_redis().flushall()
     with rq.Connection(utils.get_redis()):
-        worker = rq.worker.HerokuWorker([rq.Queue('default')],
-                                        exception_handlers=[error_handler])
+	QUEUES = [rq.Queue('default')]
+        worker = rq.worker.HerokuWorker(
+            QUEUES, exception_handlers=[retry_handler])
         worker.work()
 
 
