a9caed9ce2d963195878bcda9bcf30551205f45b
Mehdi Abaakouk <sileht@sileht.net>
2018-03-21 16:05:29 +0100

    Github website change

--- pastamaker/webhack.py
+++ pastamaker/webhack.py
@@ -26,103 +26,100 @@ from pastamaker import config
 
 LOG = logging.getLogger(__name__)
 
-global s
-s = None
+global global_session
+global_session = None
 
 
-def get_web_session():
-    s = requests.Session()
-    s.headers['User-Agent'] = (
-        'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 '
-        '(KHTML, like Gecko) Chrome/58.0.3029.96 Safari/537.36')
-    s.trust_env = False  # Don't read netrc
-    r = s.get("https://github.com/login")
-    r.raise_for_status()
-    m = re.search('<input name="authenticity_token" '
-                  'type="hidden" value="([^"]*)" />', r.text)
-    token = m.group(1)
-    r = s.post("https://github.com/session",
-               data={"commit": "Sign+in",
-                     "utf8": "✓",
-                     "authenticity_token": token,
-                     "login": config.WEBHACK_USERNAME,
-                     "password": config.WEBHACK_PASSWORD})
-    r.raise_for_status()
-    return s
+class ParsingError(Exception):
+    pass
 
 
-def web_github_get_merge_button_page(p):
-    global s
-    if not s:
-        s = get_web_session()
-    r = s.get(p.html_url + "/merge-button",
-              headers={'x-requested-with': 'XMLHttpRequest',
-                       'accept': 'text/html'})
-    if r.status_code == 404:
-        # NOTE(sileht): Maybe we got deconnected, so retry
-        s = get_web_session()
-        r = s.get(p.html_url + "/merge-button",
-                  headers={'x-requested-with': 'XMLHttpRequest',
-                           'accept': 'text/html'})
-    r.raise_for_status()
-    return s, r.text
-
-
-def _web_github_branch_status(text):
-    if '/update_branch' not in text:
-        # No update_branch form
-        return "dirty"
-    elif 'This branch is out-of-date with the base branch' in text:
-        return "behind"
-    if 'This branch is up to date.' in text:
-        return "clean"
-    else:
-        return "unknown"
-
-
-def web_github_branch_status(p):
-    s, text = web_github_get_merge_button_page(p)
-    return _web_github_branch_status(text)
-
-
-def web_github_update_branch(p):
-    s, text = web_github_get_merge_button_page(p)
-    state = _web_github_branch_status(text)
-    if state != "behind":
-        LOG.error("PR#%s: Can't update branch, state is not behind but %s",
-                  p.number, state)
-        return False
-
-    m = re.search('/update_branch" .*<input name="authenticity_token" '
-                  'type="hidden" value="([^"]*)" />', text)
+def get_authencity_token(text):
+    m = re.search('name="authenticity_token"[^>]*value="([^"]*)" />', text.replace("\n", ""))
     if not m:
-        LOG.error("PR#%s: Can't update branch, authenticity_token not found" %
-                  p.number)
-        return False
+        raise ParsingError("Authencity token not found")
+    return m.group(1)
 
-    token = m.group(1)
-    m = re.search('<input type="hidden" name="expected_head_oid" '
-                  'value="([^"]*)">', text)
+
+def get_last_modified(text):
+    m = re.search('data-last-modified="([^"]*)"', text.replace("\n", ""))
     if not m:
-        LOG.error("PR#%s: Can't update branch, head_oid not found" %
-                  p.number)
+        raise ParsingError("data-last-modified not found")
+    return m.group(1)
+
+
+def get_web_session(force=False):
+    global global_session
+    if not global_session or force:
+        global_session = requests.Session()
+        global_session.headers['User-Agent'] = (
+            'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 '
+            '(KHTML, like Gecko) Chrome/64.0.3282.119 Safari/537.36')
+        global_session.trust_env = False  # Don't read netrc
+
+        r = global_session.get("https://github.com/login")
+        r.raise_for_status()
+        token = get_authencity_token(r.text)
+        r = global_session.post("https://github.com/session",
+                   data={"commit": "Sign+in",
+                         "utf8": "✓",
+                         "authenticity_token": token,
+                         "login": config.WEBHACK_USERNAME,
+                         "password": config.WEBHACK_PASSWORD})
+        r.raise_for_status()
+    return global_session
+
+def _web_github_update_branch(p):
+    s = get_web_session()
+
+    # get the PR to set some useful cookie
+    r = s.get(p.html_url + "/merge-button")
+    r.raise_for_status()
+
+    # Ensure we can click on the btn
+    if 'This branch is out-of-date with the base branch' not in r.text:
+        LOG.error("PR#%s: Can't update branch, state is not behind", p.number)
         return False
 
-    expected_head_oid = m.group(1)
-    r = s.post(p.html_url + "/update_branch",
-               headers={
-                   'X-Requested-With': 'XMLHttpRequest',
-                   'Content-Type': 'application/x-www-form-urlencoded; '
-                   'charset=UTF-8'
-               },
-               data={"utf8": "✓",
-                     "expected_head_oid": expected_head_oid,
-                     "authenticity_token": token})
+    # Click on the Update btn
+    token = get_authencity_token(r.text)
+    r = s.post(p.html_url + "/update_branch", data={
+        "utf8": "✓",
+        "authenticity_token": token,
+        "expected_head_oid": p.head.sha,
+    }, headers={'x-requested-with': 'XMLHttpRequest'})
     r.raise_for_status()
     return True
 
 
+def web_github_update_branch(p):
+    try:
+        return _web_github_update_branch(p)
+    except ParsingError as e:
+        LOG.error("PR#%s: Can't update branch: %s",
+                  p.number, e)
+        return False
+
+def test():
+    import github
+
+    from pastamaker import utils
+    from pastamaker import gh_pr
+
+    utils.setup_logging()
+    config.log()
+    gh_pr.monkeypatch_github()
+
+    parts = sys.argv[1].split("/")
+
+    LOG.info("Getting pull request...")
+    g = github.Github()
+    user = g.get_user(parts[3])
+    repo = user.get_repo(parts[4])
+    p = repo.get_pull(int(parts[6]))
+    LOG.info("Pushing update button...")
+    LOG.info(web_github_update_branch(p))
+
+
 if __name__ == '__main__':
-    url = sys.argv[1]
-    p = mock.Mock(number=int(url.split("/")[-1]), html_url=url)
-    print(web_github_update_branch(p))
+    test()
